27d920da6906873c3c6a29a43bb2f53985c714b0
Kashish pratap Singh <singhkashishpratap@gmail.com>
2024-09-05 13:54:36 +0530

    Add a println

--- Application.kt
+++ Application.kt
@@ -1,6 +1,7 @@
 fun main() {
     var iAmount:Int=1000
     while(true){
+        println("You want to make withdraw?(yes/no): ")
         var i:String=readln();
         if(i=="yes") {
             var eAmount:Int=readln().toInt()
